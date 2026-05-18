24e53ed91dd35232fe919c0c05ff8ed51e07b438
isabele-eduarda <isabeleeduarda307@gmail.com>
2026-05-18 11:27:10 -0300

    classes admin

--- livraria/admin.py
+++ livraria/admin.py
@@ -2,7 +2,44 @@ from django.contrib import admin
 from .models import Autor, Categoria, Editora, Livro
 # Register your models here.
 
-admin.site.register(Autor)
-admin.site.register(Categoria)
-admin.site.register(Editora)
-admin.site.register(Livro)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome','descricao',)
+    search_fields = ('nome',)
+
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome','cidade','site',)
+    search_fields = ('nome',)
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome','nacionalidade',)
+    search_fields = ('nome',)
+    list_filter = ('nacionalidade',)
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = (
+    'titulo',
+    'isbn',
+    'pagina',
+    'preco',
+    'ano_publicacao',
+    'capa',
+    'estoque',
+    'disponivel',
+    'sinopse',
+    'criado_em' ,
+    'atualizado_em',
+    )
+    list_filter = ('categoria','editora','disponivel','ano_publicacao',)
+    search_fields = ('titulo', 'isbn', 'autores_nome',)
+
+
+
+
